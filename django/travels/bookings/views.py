3e694ac60d075c1c58e89b14382ba4b585e7ff29
InduTalari <indutalari17@gmail.com>
2026-07-28 08:40:56 +0530

    Add Django backend files

---
+++ django/travels/bookings/views.py
@@ -0,0 +1,83 @@
+# Create your views here.
+# authinticate,permission,token,status,respone,generics,apiviews'
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from rest_framework import status,generics 
+from rest_framework.views import APIView 
+from rest_framework.response import Response
+from .serializer import UserRegistrationSerializer,BusSerializer,BookingSerializer
+from .models import Bus,Seat,Booking
+from rest_framework.response import Response
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+
+        print(serializer.errors)   # Add this line
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(APIView):
+    def post(self,request):
+        username=request.data.get('username')
+        password=request.data.get('password')
+        user=authenticate(username=username,password=password) 
+
+        if user:
+            token,created=Token.objects.get_or_create(user=user)
+            return Response({
+                'token':token.key,
+                'user_id':user.id 
+            },  status=status.HTTP_200_OK)
+        else:
+            return Response({'error':'Invalid Credentials'},status=status.HTTP_401_UNAUTHORIZED)
+        
+class BusListCreateView(generics.ListCreateAPIView):
+    queryset=Bus.objects.all()
+    serializer_class=BusSerializer 
+
+
+class BusDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Bus.objects.all()
+    serializer_class=BusSerializer
+
+
+class BookingView(APIView):
+    permission_classes=[IsAuthenticated]
+
+    def post(self,request):
+        seat_id=request.data.get('seat')
+        try:
+            seat=Seat.objects.get(id=seat_id)
+            if seat.is_booked:
+                return Response({'error':'Seat already booked'},status=status.HTTTP_400_BAD_REQUEST)
+            
+            seat.is_booked=True
+            seat.save()
+
+            bookings=Booking.objects.create(
+                user=request.user,
+                bus=seat.bus,
+                seat=seat
+            )
+            serializer=BookingSerializer(bookings)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        except Seat.DoesNotExist:
+            return Response({'error':'Invalid Seat Id'},status=status.HTTP_400_BAD_REQUEST)
+
+class UserBookingView(APIView):
+     permission_classes=[IsAuthenticated]
+
+     def get(self,request,user_id):
+         if request.user.id!=user_id:
+             return Response({'error':'Unauthorized'},status=status.HTTP_401_UNAUTHORIZED)
+         
+         bookings=Booking.objects.filter(user_id=user_id)
+         serializer=BookingSerializer(bookings,many=True)
+         return Response(serializer.data)
+         
+             
